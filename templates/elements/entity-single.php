c57749babedacc5cb14cff93621d3f57a1616dea
Marius Miliunas <mmiliunas@jackthreads.com>
2020-12-25 22:01:39 +0200

    Fix entity images

--- templates/elements/entity-single.php
+++ templates/elements/entity-single.php
@@ -1,17 +1,21 @@
 <?php
 
 $entity = $args[ 'entity' ];
-$has_photo = $entity->get_acf_picture();
+$photo = $entity->get_acf_picture();
+if ( $photo ) {
+    $non_retina = $photo[ 'sizes' ][ 'cd_thumb' ];
+    $retina = $photo[ 'sizes' ][ 'cd_thumb@2x' ];
+}
 ?>
 
 <div class="entity col-xs-12 col-md-4 p-3 mb-3">
     <a href="<?= $entity->get_display_link( $entity ) ?>">
-    	<?php if ( $has_photo ): ?>
-	    	<div class="img-container non-retina" style="background: url(<?= $entity->get_featured( 'cd_thumb' ) ?>) no-repeat center;">
-	            <img class="d-none d-sm-none" src="<?= $entity->get_featured( 'cd_thumb' ) ?>" />
+    	<?php if ( $photo ): ?>
+	    	<div class="img-container non-retina" style="background: url(<?= $non_retina ?>) no-repeat center;">
+	            <img class="d-none d-sm-none" src="<?= $non_retina ?>" />
 	        </div>
-            <div class="img-container retina" style="background: url(<?= $entity->get_featured( 'cd_thumb@2x' ) ?>) no-repeat center;">
-                <img class="d-none d-sm-none" src="<?= $entity->get_featured( 'cd_thumb@2x' ) ?>" />
+            <div class="img-container retina" style="background: url(<?= $retina ?>) no-repeat center;">
+                <img class="d-none d-sm-none" src="<?= $retina ?>" />
             </div>
 	    <?php endif; ?>
     <div class="card p-3">
