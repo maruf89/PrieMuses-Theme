1ebfe2341975acfb90be0f81aed07d95438a49fd
Marius Miliunas <mmiliunas@jackthreads.com>
2021-02-06 14:17:55 +0200

    Update versions

--- src/Theme.php
+++ src/Theme.php
@@ -19,7 +19,7 @@ class Theme {
     public static string $template_dir = '/templates';
 
     public $themeoptions = [];
-    public string $version      = '1.1.3';
+    public string $version      = '1.1.4';
     public $themedata    = [];
 
     private string $assets_uri;
