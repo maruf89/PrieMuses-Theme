56ccaaf4fee1681a40d3db651d0a2a0f80b53d7a
Marius Miliunas <mmiliunas@jackthreads.com>
2021-02-08 09:34:30 +0200

    Fixing entity meta to strip any tags

--- src/Modules/SEO/ClassMeta.php
+++ src/Modules/SEO/ClassMeta.php
@@ -51,7 +51,7 @@ class ClassMeta {
                 $meta = get_post_meta( $post->ID ) ?? [];
                 $desc = $meta[ $acf::$entity_about ] ?? [];
                 if ( count( $desc ) ) $desc = $meta[ $acf::$entity_about ][ 0 ];
-                break;
+                return $this->_meta_description( $desc, true, true );
 
         }
         $this->_meta_description( $desc, true );
